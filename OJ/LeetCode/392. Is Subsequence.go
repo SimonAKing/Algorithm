f005394d6cfbb585b9c17bb063ba84796e14f67e
Simon Ma <simon@tomotoes.com>
2020-03-25 15:30:48 +0800

    add some solutions so

---
+++ OJ/LeetCode/392. Is Subsequence.go
@@ -0,0 +1,10 @@
+import "regexp"
+
+func isSubsequence(s string, t string) bool {
+  regex := ".*"
+  for _,i := range s {
+    regex += string(i) + ".*"
+  }
+  matched, _ := regexp.MatchString(regex, t)
+  return matched
+}
